6f4ef34b5d14bd19dc4f00bb4bb15026a43f059d
Absolent - Lexo <ba.aldana92@gmail.com>
2025-01-24 16:12:17 -0500

    Se modifica el modelo phone cambiando el nombre de la tabla

--- app/Models/Phone.php
+++ app/Models/Phone.php
@@ -6,7 +6,7 @@ use Illuminate\Database\Eloquent\Model;
 
 class Phone extends Model
 {
-    protected $table = 'phones';
+    protected $table = 'phone';
     
     protected $fillable = [
         'id_user',
