0adbf747cb51768e1243f3b579a8e6f8b64a9521
Absolent - Lexo <ba.aldana92@gmail.com>
2025-01-24 16:32:25 -0500

    Se crea el PhoneService en el cual se define inicialmente la funcion  addPhone

---
+++ app/Services/PhoneService.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace App\Services;
+
+use App\Repositories\PhoneRepository;
+use App\Traits\JsonResponse;
+
+class PhoneService {
+
+    use JsonResponse;
+
+    public $phoneRepository;
+
+    public function __construct(PhoneRepository $phoneRepository){
+        $this->phoneRepository = $phoneRepository;
+    }
+
+    public function addPhone ($data) {
+
+        $idUser = $data['id_user'];
+        $phonesData = $data['phones'];
+        $numbersAlreadyRegistered = [];
+
+        foreach ($phonesData as $phoneData) {
+
+            $prefix = $phoneData['prefix'];
+            $phoneNumber = $phoneData['number'];
+
+            $validatorNumber = $this->phoneRepository->isPhoneAlreadyRegistred($idUser, $prefix, $phoneNumber);
+
+            if ($validatorNumber) {
+                $numbersAlreadyRegistered[] = $prefix . ' ' . $phoneNumber;
+            } else {
+                $this->phoneRepository->newPhone($idUser, $prefix, $phoneNumber);
+            }
+        }
+
+        if (!empty($numbersAlreadyRegistered)) {
+            return $this->errorResponse([
+                'message' => 'Los siguientes números ya se encuentran registrados:',
+                'phoneNumbers' => $numbersAlreadyRegistered
+            ],422);
+        } else {
+            return $this->successResponse($phonesData, 201);
+        }
+        
+    }
+}
