3260101409f83da4ff30c117edefd3ed32117818
Absolent - Lexo <ba.aldana92@gmail.com>
2025-01-29 13:48:43 -0500

    Se agrega el metodo validationRoleUser y se modifica el newRoleUser cambiando los tipos de entrada de los parametros

--- app/Repositories/RoleUserRepository.php
+++ app/Repositories/RoleUserRepository.php
@@ -5,11 +5,17 @@ namespace App\Repositories;
 use App\Models\RoleUser;
 
 class RoleUserRepository {
-    public function newRoleUser(int $idUser, int $idRole) {
+    public function newRoleUser(int $userId, int $roleId) {
         return RoleUser::create([
-            'id_role' => $idRole,
-            'id_user' => $idUser
+            'id_role' => $roleId,
+            'id_user' => $userId
         ]);
+    }
+
+    public function validationRoleUser (int $userId, int $roleId) {
+        return RoleUser::where('id_role', $roleId)
+                        ->where('id_user', $userId)
+                        ->exists();
     }
 
     public function getAllRoleUser() {
